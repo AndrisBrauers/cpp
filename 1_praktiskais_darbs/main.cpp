5cc76359ee812f4a2c21a0351d68b35677a6af1a
AndrisBrauers <andris.brauers@uniso.lv>
2022-12-10 20:49:30 +0200

    Labojums 2

--- 1_praktiskais_darbs/main.cpp
+++ 1_praktiskais_darbs/main.cpp
@@ -49,43 +49,9 @@ int main(){
             break;
         default:
             cout << "Nepraeiza opcija, mēģini vēlreiz" << endl;
-            break;
+            continue;
         }
-        
-        if (input == '1') {
-            cin.ignore();
-            getline(cin, text);
-        }
-        else if (input == '2') {
-            cout << ((text.length() % 2 == 0) ? "Pāra" : "Nepāra") << endl;
-        }
-        else if (input == '3') {
-            int sum = 0;
-            for(int i = 1; i < text.length() + 1; i++){
-                sum += i;
-            }
-            cout << "Summa: " << sum << endl;
-        }
-        else if (input == '4') {
-            int fact = 1;
-            for(int i = 1; i < text.length() + 1; i++){
-                fact *= i;
-            }
-            cout << "Factoriālis: " << fact << endl;
-        }
-        else if (input == '5') {
-            for(int i = text.length(); i >= 0; i--){
-                cout << text[i];
-            }
-            cout << endl;
-        }
-        else if (input == '6') {
-            break;
-        }
-        else {
-            cout << "Nepraeiza opcija, mēģini vēlreiz" << endl;
-        }
-        
+        break;
     }
     return 0;
 }
