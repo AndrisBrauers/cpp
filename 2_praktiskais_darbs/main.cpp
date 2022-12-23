bb174538c24c8090b2d4af35781981cae802bd5b
AndrisBrauers <andrisbrauers@gmail.com>
2022-12-23 20:36:40 +0200

    2_praktiskais_darbs

---
+++ 2_praktiskais_darbs/main.cpp
@@ -0,0 +1,65 @@
+#include "my_functions.h"
+
+int main(){
+    enum menu {
+                Insert = '1', 
+                Queue = '2', 
+                Stack = '3',
+                Two_Stacks = '4',
+                Map = '5',
+                Print_Vector = '6',
+                Exit = '7'
+               };
+    char input;
+    vector<int> v_numbers {4, 5, 6, 7, 8};
+
+    while(true){
+        cout << "1: Ievadīt skaitļus\n";
+        cout << "2: Rinda\n";
+        cout << "3. Steks\n";
+        cout << "4. Divi steki (Rinda)\n";
+        cout << "5. Map\n";
+        cout << "6. Izvada skaitļu virkni\n";
+        cout << "7. Beigt darbību\n";
+
+        cin >> input;
+
+        switch (input)
+        {
+        case Insert:
+            addNum(v_numbers);
+            cout << endl;
+            continue;
+        case Queue:
+            queuePrint(v_numbers);
+            cout << endl;
+            continue;
+        case Stack:
+            stackPrint(v_numbers);
+            cout << endl;
+            continue;
+        case Two_Stacks:
+            twoStackPrint(v_numbers);
+            cout << endl;
+            continue;
+        case Map:
+            /*Ieliek vectora skaitļus Map struktūrā. Tad katrs skaitlis
+            tiek izprintēts kopā ar savu pozīciju, kā key-value pair */
+            
+            mapPrint(v_numbers);
+            cout << endl;
+            continue;
+        case Print_Vector:
+            printVector(v_numbers);
+            cout << endl;
+            continue;
+        case Exit:
+            break;
+        default:
+            cout << "Nepraeiza opcija, mēģini vēlreiz" << endl;
+            continue;
+        }
+        break;
+    }
+    return 0;
+}
---
+++ 2_praktiskais_darbs/my_functions.h
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+void addNum(vector<int> &vect) {
+    string text = "";
+    string num = "";
+    cin.ignore();
+    getline(cin, text);
+    
+    for (int i = 0; i < text.length(); i++) {
+        if (int(text[i]) > 47 && int(text[i]) < 58) {
+            num += text[i];
+        } else if (num != "") {
+            vect.push_back(stoi(num));
+            num = "";
+        }
+    }
+    if (num != "") {
+        vect.push_back(stoi(num));
+    }
+}
+
+void queuePrint(vector<int> &vect) {
+    queue<int> q_numbers;
+    for (auto i = vect.begin(); i != vect.end(); ++i) {
+        q_numbers.push(*i);
+    }
+
+    while (!q_numbers.empty()) {
+        cout << q_numbers.front() << ' ';
+        q_numbers.pop();
+    }
+}
+
+void stackPrint(vector<int> &vect) {
+    stack<int> s_numbers;
+    for (auto i = vect.begin(); i != vect.end(); ++i) {
+        s_numbers.push(*i);
+    }
+
+    while (!s_numbers.empty()) {
+        cout << s_numbers.top() << ' ';
+        s_numbers.pop();
+    }
+}
+
+void twoStackPrint(vector<int> &vect) {
+    stack<int> s1_numbers;
+    stack<int> s2_numbers;
+    for (auto i = vect.begin(); i != vect.end(); ++i) {
+        s1_numbers.push(*i);
+    }
+
+    while (!s1_numbers.empty()) {
+        s2_numbers.push(s1_numbers.top());
+        s1_numbers.pop();
+    }
+
+    while (!s2_numbers.empty()) {
+        cout << s2_numbers.top() << ' ';
+        s2_numbers.pop();
+    }
+}
+
+void mapPrint(vector<int> &vect) {
+    map<int, int> m_numbers;
+    for (int i = 0; i != vect.size(); i++) {
+        m_numbers.insert(pair<int, int>(i, vect[i]));
+    }
+    for (auto const &tupl: m_numbers) {
+        cout << tupl.first << " : " << tupl.second << endl;
+    }
+}
+
+void printVector(vector<int> &vect) {
+    for (auto i = vect.begin(); i != vect.end(); ++i) {
+        cout << *i << " ";
+    }
+}
